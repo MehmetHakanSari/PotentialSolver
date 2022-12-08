9fc71574cfb693b5264aebf543ef3e427761e66e
MehmetHakanSari <m.hakansari@outlook.com>
2022-12-08 11:41:10 +0300

    If statemnt is included for north BC indexing

--- Domain.py
+++ Domain.py
@@ -452,15 +452,17 @@ class PDE_2D_Solver:
                 # print(W)
                 # print(C)
                 # print(E)
-                print(Q)
+                # print(Q)
         
                 # phi[-2:0:-1,i] = self.TDMA(W,C,E,Q)  #interior points are calculated in reverse order. It is because of matrix index convention. 
                 # print(phi[y_index[-1]::-1,i])
                 # print(phi[y_index[-1]:y_index[0] - 1:-1,i])
                 # print(self.TDMA(W,C,E,Q) )
 
-                # phi[y_index[-1]:y_index[0] - 1:-1,i] = self.TDMA(W,C,E,Q) 
-                phi[y_index[-1]::-1,i] = self.TDMA(W,C,E,Q) 
+                if y_index[0] == 0:
+                    phi[y_index[-1]::-1,i] = self.TDMA(W,C,E,Q) 
+                else:
+                    phi[y_index[-1]:y_index[0] - 1:-1,i] = self.TDMA(W,C,E,Q) 
 
                 # print("center")
                 # print(C)
