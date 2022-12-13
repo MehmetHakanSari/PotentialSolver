8fec710bd81575f365306b549a41830dd53d3bd3
MehmetHakanSari <m.hakansari@outlook.com>
2022-12-13 10:10:53 +0300

    OneDcentralldiff function is added

--- Differentials.py
+++ Differentials.py
@@ -27,6 +27,78 @@ def TwoDcentraldiff(func, dx, dy):
 
     return dfuncdx, dfuncdy
 
+def OneDcentraldiff(func, dx, axis = 0):
+    """
+        Function: 1D or 2D array. Functions derivative is taken w.r.t given spacing. 
+        dx      : float or 1D array. Spacing of grid.
+        axis    : the direction of the derivative. 0 is row derivative for 2D array. 1 is column derivatives.  
+
+        Default setting is: 
+
+        func: 1 x N
+        dx:   1 x (N-1)
+    """
+    (m, n) = np.shape(func)
+    if axis == 0:
+        Operation = "row"
+    else:
+        Operation = "column"
+    
+    divflag = True
+    if type(dx) == float:
+        divflag = False
+    else:
+        (k,l) = np.shape(dx)
+        
+        # func: 1D array
+        if m > 1 and n == 1:        #if func is column vector
+            Operation = "column"
+            if l > k:               #but dx is given as row vector
+                dx = dx.T
+        elif n > 1 and m == 1:  #if func is row vector
+            if k > l:           #but dx is given as column vector
+                dx = dx.T
+    
+    if Operation == "row":
+        dfuncdx = np.zeros((m,n), dtype="float")
+
+        if divflag:
+            dfuncdx[:,0] = (-func[:,2] + 4 * func[:,1] - 3 * func[:,0]) / (dx[0] + dx[1])
+            dfuncdx[:,1:-1] = (func[2:] - func[0:-2]) / (dx[0:-1] + dx[1:])
+            dfuncdx[:,0] = (-func[:,2] + 4 * func[:,1] - 3 * func[:,0]) / (dx[-1] + dx[-2])
+        else:
+            dfuncdx[:,0] = (-func[:,2] + 4 * func[:,1] - 3 * func[:,0]) / (2 * dx)
+            dfuncdx[:,1:-1] = (func[2:] - func[0:-2]) / (2 * dx)
+            dfuncdx[:,0] = (-func[:,2] + 4 * func[:,1] - 3 * func[:,0]) / (2 * dx) 
+
+        return dfuncdx
+
+    elif Operation == "column":
+        dfuncdy = np.zeros((m,n), dtype="float")
+        if l > k:               #if dx is given as row vector
+            dx = dx.T
+
+        dy = dx    #last elements should be 0 for y axis.
+
+        if divflag:
+            dfuncdy[0,:] = (func[2,:] - 4 * func[1,:] + 3 * func[0,:]) / (dy[0] + dy[1])
+            dfuncdy[1:-1,:] = (-func[2:,:] + func[0:-2,:]) / (dy[0:-1] + dy[1:])
+            dfuncdy[-1,:] = (-func[-3,:] + 4 * func[-2, :] - 3 * func[-1,:]) / (dy[-1] + dy[-2])
+        else:
+            dfuncdy[0,:] = (func[2,:] - 4 * func[1,:] + 3 * func[0,:]) / (2 * dy)
+            dfuncdy[1:-1,:] = (-func[2:,:] + func[0:-2,:]) / (2 * dy)
+            dfuncdy[-1,:] = (-func[-3,:] + 4 * func[-2, :] - 3 * func[-1,:]) / (2 * dy)
+
+        return dfuncdy
+
+
+
+
+
+
+
+
+
 def TDMA(W,C,E,Q):
     """
         W: west
