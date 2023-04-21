96b546bd34852949c9a6bf3f92103f5baa8d9094
MehmetHakanSari <m.hakansari@outlook.com>
2023-04-21 19:48:38 +0300

    Search algorithm is writtedn as weel as xfoil is added

--- VekilModel/AirfoilClassifier/classifier_objects.py
+++ VekilModel/AirfoilClassifier/classifier_objects.py
@@ -20,6 +20,7 @@ class Airfoil:
         self.name = name
         self.geometry = geometry
         self.polars = polars
+        self.data_available = data_available
 
     def __call__():
         """
@@ -54,5 +55,54 @@ class Airfoil:
         It needs a datapath to be defined.
         """
         pass
+
+    def polar_search(self, Re, alpha_range, **kwargs):
+        """
+        Search for the closest polar for given Re.
+        """
+        
+        if Re not in self.polars:
+            return None
+        else:
+            polar = self.polars[Re]
+            #check the AoA range from given kwargs and applied it to polar
+            #before check wheter airfoil has datas for given alpha range
+
+            if not(polar[:,0].min() >= alpha_range[0] or polar[:,0].max() <= alpha_range[1]):
+
+                polar = polar[np.logical_and(polar[:,0] >= alpha_range[0], polar[:,0] <= alpha_range[1])]
+                lift_statment = True
+                drag_statment = True
+                moment_statment = True
+                pressure_statment = True
+                ClCd_statment = True
+                dClda_statment = True
+                if "Cl" in kwargs:
+                    Cl = kwargs["Cl"]
+                    lift_statment = polar[:,1].min() >= Cl
+                if "Cd" in kwargs:
+                    Cd = kwargs["Cd"]
+                    drag_statment = polar[:,2].max() <= Cd
+                if "Cm_range" in kwargs:
+                    Cm_range = kwargs["Cm_range"]
+                    moment_statment = np.logical_and(polar[:,3] >= Cm_range[0], polar[:,3] <= Cm_range[1]).any()
+                if "Cp_range" in kwargs:
+                    Cp_range = kwargs["Cp_range"]
+                    pressure_statment = np.logical_and(polar[:,4] >= Cp_range[0], polar[:,4] <= Cp_range[1]).any()
+                if "ClCd_range" in kwargs:
+                    ClCd_range = kwargs["ClCd_range"]
+                    ClCd_statment = np.logical_and(polar[:,5] >= ClCd_range[0], polar[:,5] <= ClCd_range[1]).any()
+                if "dClda_range" in kwargs:
+                    dClda_range = kwargs["dClda_range"]
+                    dClda = (polar[0:-1,1] - polar[1:,1]) / (polar[0:-1,0] - polar[1:,0]) 
+                    dClda_statment = np.logical_and(dClda >= dClda_range[0], dClda <= dClda_range[1]).any()
+                
+                if lift_statment and drag_statment and moment_statment and pressure_statment and ClCd_statment and dClda_statment:
+                    return True
+                
+            else:
+                return False
+
+        return None
 
 
---
+++ VekilModel/AirfoilClassifier/seach_foil.py
@@ -0,0 +1,43 @@
+import numpy as np
+import os
+import pickle
+from classifier_objects import Airfoil
+
+#The function will search for thr airfoil for given specific Cl, Cd, Cm for given Re and AoA
+
+def search_foil(Re, alpha_range, data_path, **kwargs):
+    """
+        Re: Reynolds number, float
+        alpha_range: range of alpha, list
+        Cl: minimum lift coefficient, float
+        Cd: maximum drag coefficient, float
+        Cm: moment coefficient, list
+        Cp: pressure coefficient, list
+        ClCd: lift to drag ratio, list
+    """
+    #The user should give the specific Cl, Cd for given range of AoA and Re.
+    #The function will search for the airfoil that has the closest Cl, Cd, Cm for given Re and AoA
+
+    condition_satisfied_airfoils = []
+    # print(os.listdir(data_path))
+    #search for folder names including given Re
+    Re_folders = [f for f in os.listdir(data_path) if str(Re) and "pkl" in f]
+    #import airfoils using pickle in each Re folder
+    for Re_folder in Re_folders:
+        airfoils = []
+        for file in os.listdir(data_path + Re_folder):
+            if file.endswith(".pkl"):
+                with open(data_path + Re_folder + "/" + file, "rb") as f:
+                    airfoil = pickle.load(f)
+                    if airfoil.data_available:
+                        airfoils.append(airfoil)
+        #search for the airfoil that has the closest Cl, Cd, Cm for given Re and AoA
+        for airfoil in airfoils:
+            if airfoil.polar_search(Re, alpha_range, **kwargs):
+                condition_satisfied_airfoils.append(airfoil)
+
+    return condition_satisfied_airfoils
+
+    
+
+
